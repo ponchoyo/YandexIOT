57f6f798b2fc418ce9e4395a45905b51320a5b95
Anton Volodin <989728@gmail.com>
2023-01-08 17:39:51 +0300

    Update __init__.py

--- src/YandexIOT/Devices/__init__.py
+++ src/YandexIOT/Devices/__init__.py
@@ -1,9 +1,11 @@
 from .SmartDevice import SmartDevice
 from .SmartLight import SmartLight
 from .SmartVacuum import SmartVacuum
+from .SmartSocket import SmartSocket
 
 
 TYPES = {
     "devices.types.light": SmartLight,
     "devices.types.vacuum_cleaner": SmartVacuum,
+    "devices.types.socket": SmartSocket,
 }
